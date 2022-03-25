716889335243778f8a90df62dd8aa61d50e9eb96
Halyna Urbanska <aquilla@ukr.net>
2022-03-25 15:23:40 +0200

    Add db and create new record

--- server/test/api.test.js
+++ server/test/api.test.js
@@ -14,12 +14,41 @@ describe('GET /api/v1', () => {
   });
 });
 
-describe('GET /api/v1/emojis', () => {
-  it('responds with a json message', (done) => {
+describe('POST /api/v1/messages', () => {
+  it('responds with inserted message', (done) => {
+    const result = {
+      name: 'Test Name',
+      message: 'Test message',
+      latitude: 10,
+      longitude: -10,
+    };
     request(app)
-      .get('/api/v1/emojis')
+      .post('/api/v1/messages')
+      .send(result)
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
-      .expect(200, ['😀', '😳', '🙄'], done);
+      .expect((res) => {
+        delete res.body._id;
+        delete res.body.date;
+      })
+      .expect(200, result, done);
+  });
+  it('can signup that has diacritics', (done) => {
+    const result = {
+      name: 'Ÿööhöö',
+      message: 'Test message',
+      latitude: 10,
+      longitude: -10,
+    };
+    request(app)
+      .post('/api/v1/messages')
+      .send(result)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect((res) => {
+        delete res.body._id;
+        delete res.body.date;
+      })
+      .expect(200, result, done);
   });
 });
