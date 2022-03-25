785f3bd5239154bcf57dd5da8d82b6b69cc326f4
Halyna Urbanska <aquilla@ukr.net>
2022-03-25 19:13:30 +0200

    Resolve conflicts

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -27,7 +27,7 @@ export interface IKeyValueObject {
 export const App = (): ReactElement => {
   const [ coords, setCoords ] = useState<ICoords>(center);
   const [ waitLocation, setWaitLocation ] = useState(true);
-  const [ messages, setMessages ] = useState<IMessage[]>([]);
+  const [ messages, setMessages ] = useState<IKeyValueObject>({});
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((pos) => {
